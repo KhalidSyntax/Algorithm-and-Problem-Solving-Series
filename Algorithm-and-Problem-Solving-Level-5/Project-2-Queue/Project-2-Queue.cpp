db81e5a3aa12ab181647d48f613d7c093d2aa678
KhalidSyntax <amrikhalid2024@gmail.com>
2026-06-07 13:31:17 +0300

    Create Queue Project

---
+++ Algorithm-and-Problem-Solving-Level-5/Project-2-Queue/Project-2-Queue.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "clsMyQueue.h";
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+
+    //------------------------------------
+    //------------------------------------
+
+    clsDblLinkedList<int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(50);
+    MydblLinkedList.InsertAtBeginning(40);
+    MydblLinkedList.InsertAtBeginning(30);
+    MydblLinkedList.InsertAtBeginning(20);
+    MydblLinkedList.InsertAtBeginning(10);
+
+    cout << "\nLinked List Content:\n";
+    MydblLinkedList.PrintList();
+
+    cout << "\nDetails:\n";
+    MydblLinkedList.PrintListDetails();
+
+    MydblLinkedList.InsertAfter(2, 700);
+
+    cout << "\nLinked List Content After Insert By Index (2):\n";
+    MydblLinkedList.PrintList();
+
+    cout << "\nDetails:\n";
+    MydblLinkedList.PrintListDetails();
+
+    system("pause>0");
+    return 0;
+
+
+
+
+
+
+
+
+}
